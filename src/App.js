1a7185f2b218910b07781e3da48d1d914f969ca9
yan0203 <62398435+yan0203@users.noreply.github.com>
2020-06-21 16:10:51 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/*Dentor do React nos não podemos manipular a variável do nosso State */
+import React from 'react';
+import './global.css';
+
+import Routes from './routes'
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
